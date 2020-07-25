d4aec71ad6f2c38fb3762fabd130505af94d6dd7
Hyunsu Joo <hs9880@gmail.com>
2020-07-25 08:03:51 -0400

    add API endpoint for getting single user doc

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,13 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
 const {catchErrors} = require('../handlers/errorHandlers');
+const {asyncMiddleware} = require('../utils/async');
 
 
-
-
-router.get('/',userController.getUsers);
-router.post('/signup',userController.signup);
+router.get('/',userController.getUsers); //get All user
+router.post('/signup',userController.signup); //get single user
 router.post('/login',userController.login);
-
+router.get('/:id',userController.getUserDoc);
 
 
 
