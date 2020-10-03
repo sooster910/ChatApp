a85404345f0d798a63642b4c3ae6b8158054afdc
Hyunsu Joo <hs9880@gmail.com>
2020-10-04 00:04:12 +0900

    Add emoji for chat message

--- client/src/components/ChatRoom.js
+++ client/src/components/ChatRoom.js
@@ -1,9 +1,9 @@
 import React, { Component, createRef } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import Picker from 'emoji-picker-react';
 
-
-const ChatRoom = ({ messages, onChange, sendMessage, message, onKeyPress }) => {
+const ChatRoom = ({ messages, onChange, sendMessage, message, onKeyPress, onEmojiClick}) => {
   return (
     <React.Fragment>
       {messages.length === 0 ? (
@@ -17,12 +17,13 @@ const ChatRoom = ({ messages, onChange, sendMessage, message, onKeyPress }) => {
         )}
       <div className="chatRoom_input_wrapper">
         {/* <InputOptionList /> */}
-
+        {/* <Emoji setMeesage={setMessage} message={message} />   */}
+        <Picker onEmojiClick={onEmojiClick} />
         <div className="chatRoom_input_box">
           <textarea type="text" name="message" value={message} placeholder="Type something to send" onChange={onChange} onKeyPress={onKeyPress} />
           <button className="send_button" onClick={sendMessage}><FontAwesomeIcon icon={faPaperPlane} /></button>
         </div>
-
+     
      
       </div>
     </React.Fragment>
